d8f07e588c0d85e1b8f023d6dcd4208bc8444e03
yuripbong <kyri200350@gmail.com>
2025-02-06 23:48:45 +0900

    [feat] Add Audience class with Bag

---
+++ chapter01/src/main/java/org/eternity/theater/step01/Audience.java
@@ -0,0 +1,15 @@
+package org.eternity.theater.step01;
+
+public class Audience {
+    private Bag bag; // 관객의 가방
+
+    // 관객은 무조건 가방을 가져야 함
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    // 관객의 가방 조회
+    public Bag getBag() {
+        return bag;
+    }
+}
