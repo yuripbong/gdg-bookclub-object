0c30e9fc3780f7bb9d77ada8d1bb8391f643efb7
yuripbong <kyri200350@gmail.com>
2025-02-07 20:43:59 +0900

    [test] copy step01 content to step02

---
+++ chapter01/src/main/java/org/eternity/theater/step02/Bag.java
@@ -0,0 +1,45 @@
+package org.eternity.theater.step02;
+
+
+public class Bag {
+    private Long amount; // 현금
+    private Invitation invitation; // 초대장
+    private Ticket ticket; // 티켓
+
+    // 현금만 있을 경우
+    public Bag(long amount) {
+        this(null, amount);
+    }
+
+    // 현금과 초대장을 함께 갖고 있는 경우
+    public Bag(Invitation invitation, long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+
+    // 초대장 확인
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    // 티켓 확인
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    // 가방에 티켓 저장
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    // 현금 차감
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    // 현금 증가
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
+
---
+++ chapter01/src/main/java/org/eternity/theater/step02/Ticket.java
@@ -0,0 +1,9 @@
+package org.eternity.theater.step02;
+
+public class Ticket {
+    private Long fee; // 티켓 가격
+
+    public Long getFee() {
+        return fee;
+    }
+}
