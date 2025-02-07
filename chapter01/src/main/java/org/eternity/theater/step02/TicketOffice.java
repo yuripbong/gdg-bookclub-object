1519df291984b5c74e19d4e3a7681f7176f12098
yuripbong <kyri200350@gmail.com>
2025-02-07 22:35:43 +0900

    [refactor] TicketOffice capsulation

--- chapter01/src/main/java/org/eternity/theater/step02/TicketOffice.java
+++ chapter01/src/main/java/org/eternity/theater/step02/TicketOffice.java
@@ -13,12 +13,12 @@ public class TicketOffice {
         this.tickets.addAll(Arrays.asList(tickets));
     }
 
-    public Ticket getTicket() {
-        return tickets.remove(0);
+    public void sellTicketTo(Audience audience) {
+        plusAmount(audience.buy(getTicket()));
     }
 
-    public void minusAmount(Long amount) {
-        this.amount -= amount;
+    public Ticket getTicket() {
+        return tickets.remove(0);
     }
 
     public void plusAmount(Long amount) {
--- chapter01/src/main/java/org/eternity/theater/step02/TicketSeller.java
+++ chapter01/src/main/java/org/eternity/theater/step02/TicketSeller.java
@@ -1,15 +1,15 @@
 package org.eternity.theater.step02;
 
+import org.eternity.theater.step02.TicketOffice;
+
 public class TicketSeller {
     private TicketOffice ticketOffice;
 
-    // 판매원이 일하는 매표소
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
-    // 판매원이 매표소에 직접 접근
     public void sellTo(Audience audience) {
-        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+        ticketOffice.sellTicketTo(audience);
     }
 }
