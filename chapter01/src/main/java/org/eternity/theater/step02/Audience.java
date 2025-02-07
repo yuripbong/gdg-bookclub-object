82f6badc8c17f0885a25756a86ec5a6a41c56040
yuripbong <kyri200350@gmail.com>
2025-02-07 22:32:07 +0900

    [refactor] Bag capsulation

--- chapter01/src/main/java/org/eternity/theater/step02/Audience.java
+++ chapter01/src/main/java/org/eternity/theater/step02/Audience.java
@@ -3,6 +3,10 @@ package org.eternity.theater.step02;
 public class Audience {
     private Bag bag; // 관객의 가방
 
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
     // 티켓 구매
     public Long buy(Ticket ticket) {
         return bag.hold(ticket);
