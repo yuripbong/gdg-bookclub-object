e38c07553696e684d4bbb11e717848e00b7bb098
yuripbong <kyri200350@gmail.com>
2025-02-12 00:23:58 +0900

    [feat] Add PeriodCondition class

---
+++ chapter02/src/main/java/org/eternity/movie/step01/PeriodCondition.java
@@ -0,0 +1,22 @@
+package org.eternity.movie.step01;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class PeriodCondition implements DiscountCondition {
+    private DayOfWeek dayOfWeek; // 상영요일
+    private LocalTime startTime; // 시작 시간
+    private LocalTime endTime; // 종료 시간
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.getStartTime().getDayOfWeek().equals(dayOfWeek) &&
+                startTime.compareTo(screening.getStartTime().toLocalTime()) <= 0 &&
+                endTime.compareTo(screening.getStartTime().toLocalTime()) >= 0;
+    }
+}
