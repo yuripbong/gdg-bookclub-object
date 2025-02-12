e5ade42ed94ecfbd74296d782ab30fa2a3a93dad
yuripbong <kyri200350@gmail.com>
2025-02-12 20:35:25 +0900

    [feat] Add NoneDiscountPolicy class

---
+++ chapter02/src/main/java/org/eternity/movie/step01/pricing/NoneDiscountPolicy.java
@@ -0,0 +1,13 @@
+package org.eternity.movie.step01.pricing;
+
+import org.eternity.movie.step01.DiscountPolicy;
+import org.eternity.movie.step01.Money;
+import org.eternity.movie.step01.Screening;
+
+// 할인되지 않는 영화 계산
+public class NoneDiscountPolicy extends DiscountPolicy {
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
