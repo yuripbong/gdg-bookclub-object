e339fc86ec0fed5bed7135f104075d0fba74bc76
yuripbong <kyri200350@gmail.com>
2025-02-13 15:03:52 +0900

    [feat] Add Customer class with name and id

---
+++ chapter02/src/main/java/org/eternity/movie/step01/Customer.java
@@ -0,0 +1,11 @@
+package org.eternity.movie.step01;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
